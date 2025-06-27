bf929cf70bfda1b62368e894929651bd82064aba
권용준 <yoyo91828h@gmail.com>
2025-06-27 16:41:54 +0900

    feat: Lv1-2 nickname 추가

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    // N+1 문제 해결하기  JOIN -> JOIN FETCH 패치를 추가해 전체적으로 조회가 되도록 수정하였음
+    @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
