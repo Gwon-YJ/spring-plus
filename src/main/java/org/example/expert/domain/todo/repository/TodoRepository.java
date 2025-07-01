513d934d66deb4ecabad5799066105aea6a68af4
권용준 <yoyo91828h@gmail.com>
2025-07-01 17:31:57 +0900

    feat: Lv1-3 JPA의 이해

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -8,25 +8,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoCustomRepository {
+    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u " +
+            "WHERE (:weather IS NULL OR t.weather = :weather) " +
+            "AND (:start IS NULL OR t.modifiedAt >= :start) " +
+            "AND (:end IS NULL OR t.modifiedAt <= :end) " +
+            "ORDER BY t.modifiedAt DESC")
+    Page<Todo> searchTodosWithConditions(@Param("weather") String weather, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end, Pageable pageable);
 
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
-    //weather를 기준으로 일정 전체 검색
-    @Query("select t from Todo t where t.weather = :weather")
-    Page<Todo> findAllByWeather(@Param("weather") String weather, Pageable pageable);
-
-    //수정일을 기준으로 특정 기간 일정 찾기
-    @Query("select t from Todo  t where t.modifiedAt >= :startDate And t.modifiedAt<= :endDate order by t.modifiedAt desc")
-    Page<Todo> findSchedulesBetweenDates(Pageable pageable,
-                                         @Param("startDate") LocalDateTime startDate,
-                                         @Param("endDate") LocalDateTime endDate);
 
 }
