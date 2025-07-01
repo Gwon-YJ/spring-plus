01bf5eac3c0f66939eba5a931411e136ad6222ff
권용준 <yoyo91828h@gmail.com>
2025-07-01 19:20:54 +0900

    feat: 주석 추가

--- src/main/java/org/example/expert/domain/todo/repository/TodoCustomRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoCustomRepository.java
@@ -5,7 +5,7 @@ import org.example.expert.domain.todo.entity.Todo;
 
 import java.util.Optional;
 
-//
+// QueryDSL를 위한 인터페이스 추가
 public interface TodoCustomRepository {
     Optional<Todo> findByIdWithUser(Long todoId);
 }
